09611ceb8f60a2a0e14b1a6a66c2233064e1a261
lexiehelwig <plumlexie@gmail.com>
2020-02-28 11:57:40 -0500

    Server 2 homework

--- homework html/exercise server/first.js
+++ homework html/exercise server/first.js
@@ -1,35 +1,20 @@
+//Basic Server Part 1
 const http = require('http');
-const path = require('path');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('<h1>Hello, World!</h1>')
+    if (req.url === '/'){
+        res.write(`Hello, World!`);
+        res.end();
+    }
+
+    if (req.url === '/books') {
+        res.write(JSON.stringify(['Harry Potter', 'Pride & Prejudice', 'The Screwtape Letters']));
+        res.end();
     }
 });
 
-const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-//Basic Server
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-//     if (req.url === '/'){
-//         res.write(`Hello, World!`);
-//         res.end();
-//     }
-
-//     if (req.url === '/books') {
-//         res.write(JSON.stringify(['Harry Potter', 'Pride & Prejudice', 'The Screwtape Letters']));
-//         res.end();
-//     }
-// });
-
-
-// server.listen(8080);
-// console.log('server is on');
+server.listen(8080);
+console.log('server is on');
 
 
---
+++ homework html/exercise server/server2.js
@@ -0,0 +1,31 @@
+//Server Pt 2
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) {
+                console.log('There\'s an error so fix it, Lexie')
+            };
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+        );
+        // res.end('<h1>Hello, World!</h1>')
+    }
+
+    if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html'});
+            res.end(content);
+        }
+        );
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
