ef507cfb1eaf1a26a336d8c2984e0e704623d97d
lexiehelwig <plumlexie@gmail.com>
2020-04-24 05:35:09 -0400

    added dark mode functionality

--- darkmode.js
+++ darkmode.js
@@ -1,51 +1,7 @@
-// //Darkmode
-// let darkMode = localStorage.getItem('darkMode');
-// const darkModeToggle = document.querySelector('#dark-mode');
+//Dark Mode
+function darkMode() {
+    document.body.classList.toggle('dark');
+}
 
 
-// //check if dark mode is enabled
-// // if enabled, turn it off
-// // if disabled, turn it on
 
-// const enableDarkMode = () => {
-//     // add the class darkmode to the body
-//     document.body.classList.add('darkmode');
-//     // update darkmode in localStorage
-//     localStorage.setItem('darkMode', 'enabled');
-// }
-
-// const disableDarkMode = () => {
-//     // remove the class darkmode to the body
-//     document.body.classList.remove('darkmode');
-//     // update darkmode in localStorage
-//     localStorage.setItem('darkMode', 'null');
-// };
-
-
-// darkModeToggle.addEventListener('click', () => {
-//     if (darkMode !== 'enabled') {
-//         enableDarkMode();
-//         console.log(darkMode);
-//     } else {
-//         disableDarkMode();
-//     }
-// });
-
-
-//Fizzbuzz Coding Q
-// var i=100;
-
-// function fizzBuzz(i){
-//     for (i=1; i<101; i++){
-//         if (i%5 == 0 && i%3 == 0){
-//             console.log('fizzbuzz')
-//         } else if (i%5 == 0){
-//             console.log('fizz')
-//         } else if (i%3 == 0){
-//             console.log('buzz')
-//         } else {
-//             console.log(i);
-//         }
-//     }
-// }
-// fizzBuzz();
